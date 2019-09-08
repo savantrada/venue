5767f66a878237cebb0ac888ed46d61a72a2cc3b
Savan Trada <savantrada1022@gmail.com>
2019-09-07 23:54:54 -0400

    Updates

---
+++ precache-manifest.76baa96a9a28cce676197bad5a197a05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fa96ecb1a52b3a27199b488281b00b4",
+    "url": "/venue/index.html"
+  },
+  {
+    "revision": "d3b2909780f58e70c4d1",
+    "url": "/venue/static/css/main.d6fa7445.chunk.css"
+  },
+  {
+    "revision": "da8d0925fbfe61993952",
+    "url": "/venue/static/js/2.f0addddd.chunk.js"
+  },
+  {
+    "revision": "d3b2909780f58e70c4d1",
+    "url": "/venue/static/js/main.90e3bc2e.chunk.js"
+  },
+  {
+    "revision": "a6d842672eaf54a4917b",
+    "url": "/venue/static/js/runtime~main.3a60411a.js"
+  }
+]);
